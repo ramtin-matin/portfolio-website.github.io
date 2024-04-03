ba4d2d453c0811b44f4d44065c07e20f00b8c873
Ramtin Matin <ramtinmatin@Ramtins-MacBook-Air-2.local>
2024-04-03 18:13:01 -0400

    made project page

--- app.js
+++ app.js
@@ -91,3 +91,26 @@ function sendMail() {
 }
 
 
+// projects slideshow code
+var slideIndex = 1;
+showSlide(slideIndex);
+
+// Function to change the slide
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+}
+
+// Function to control which slide is shown
+function showSlide(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+
+
